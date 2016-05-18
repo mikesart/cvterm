769ae9f84f0125b0bd1d483a3db59079df6ddc0c
Michael Sartain <mikesart@gmail.com>
2016-05-18 14:47:14 -0700

    Update update_files.sh script

--- qtcreator/update_files.sh
+++ qtcreator/update_files.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
 
+PROJNAME=cvterm
+
 BASEDIR=$(dirname $0)
 cd $BASEDIR
-find .. -type f -iregex '.*\.\(c\|cxx\|cpp\|h\|lpp\|ypp\|sh\|txt\)$' -or -iname Makefile > nvterm.files
-find ../../libvterm -type f -iregex '.*\.\(c\|cxx\|cpp\|h\|lpp\|ypp\|sh\|txt\)$' -or -iname Makefile >> nvterm.files
+
+DIRS=(.. ../../libvterm)
+
+# Create blah.creator file if it doesn't exist
+if [[ ! -f ${PROJNAME}.creator ]]; then echo -e "[General]\n" > ${PROJNAME}.creator; fi
+
+# Create our defines file
+DEFINES=$(cat <<'END_HEREDOC'
+#define __LINUX__ 1
+#define _GNU_SOURCE 1
+#define HAVE_CURSES_H 1
+#define HAVE_CONFIG_H 1
+#define CLOG_MAIN 1
+END_HEREDOC
+)
+echo "${DEFINES}" > ${PROJNAME}.config
+
+> ${PROJNAME}.files
+for i in ${DIRS[@]}; do
+    echo Checking ${i}
+
+    find ${i} -type f -iregex '.*\.\(c\|cxx\|cpp\|h\|lpp\|ypp\|sh\|txt\)$' -or -iname Makefile >> ${PROJNAME}.files
+done
+
+find /usr/include -iname curses.h >> ${PROJNAME}.files
+
+fgrep -i --color=never ".h" *.files  | xargs -I{} dirname {} | sort | uniq > ${PROJNAME}.includes
+
+wc -l ${PROJNAME}.files
+wc -l ${PROJNAME}.includes
